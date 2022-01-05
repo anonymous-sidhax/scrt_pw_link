6eff72f14d92db47a988104feebc13d50c959d35
siddeshshewde <siddesh.shewde@gmail.com>
2022-01-05 03:19:16 -0500

    Encrypt Password using Fernet.

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from cryptography.fernet import Fernet
+
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
 
 def get_secret_link(request):
     secret_key = request.POST.get('secret_key')
+
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+
+    encrypted_secret = fernet.encrypt(secret_key.encode())
+
+    decrypted_secret = fernet.decrypt(encrypted_secret).decode()
+    print ("Secret Key: " + secret_key + "\nEncrypted Text: " + str(encrypted_secret) + "\nDecrypted Text: " + decrypted_secret + "\nKey: " + str(key))
     return HttpResponse("Secret Key: %s" %secret_key)
