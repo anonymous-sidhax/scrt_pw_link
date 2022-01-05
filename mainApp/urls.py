9aea6626952de9300a064954f65486aa8a282a2a
siddeshshewde <siddesh.shewde@gmail.com>
2022-01-04 19:07:12 -0500

    Passing Secret Key to backend

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('send-pass', views.get_secret_link, name='get_secret_link'),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+def get_secret_link(request):
+    secret_key = request.POST.get('secret_key')
+    print (secret_key)
+    return HttpResponse("Secret Key: ")
